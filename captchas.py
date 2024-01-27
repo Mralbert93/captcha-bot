1a1b61906fcac32ca3f224c685a81ce9e37ffaf1
Matthew Albert <61246987+Mralbert93@users.noreply.github.com>
2024-01-27 10:06:22 -0500

    Create captchas.py

---
+++ captchas.py
@@ -0,0 +1,25 @@
+import random
+import random
+import string
+from io import BytesIO
+from captcha.image import ImageCaptcha
+from PIL import Image
+import os
+
+def generate_captcha(captcha_length, characters_and_numbers):
+    if characters_and_numbers == True:
+        style = string.ascii_uppercase + string.digits
+    else:
+        style = string.ascii_uppercase
+    random_string = ''.join(random.choices(style, k=captcha_length))
+
+    captcha = ImageCaptcha()
+    data: BytesIO = captcha.generate(random_string)
+
+    image = Image.open(data)
+    image.save(f'captchas/{random_string}.png')
+
+    return random_string
+
+def delete_captcha(string):
+    os.remove(f'captchas/{string}.png')
