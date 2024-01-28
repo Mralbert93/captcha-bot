925b98b1328fe1de078198f2fca17a971dcbf333
Matthew Albert <61246987+Mralbert93@users.noreply.github.com>
2024-01-28 17:16:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -244,14 +244,13 @@ async def on_message(message):
 
     if message.channel.id == 1201256347430289619 and message.author.bot:
         user = await bot.fetch_user(message.content)
-        player = players.find_one({"id": user.id})
-        print(player)
+        player = players.find_one({"_id": user.id})
         if player is None:
             await user.send(f"Thank you for voting for <@1200756820403306586> on Top.GG.\nIn order to eligible for vote rewards, please play a game.\n{user.mention}") 
             return
         else:
-            players.update_one({"id": player_id}, {"$inc": {"extra_lives": 10}})
-            player = players.find_one({"id": player_id})
+            players.update_one({"_id": player_id}, {"$inc": {"extra_lives": 10}})
+            player = players.find_one({"_id": player_id})
             extra_lives = player["extra_lives"]
             await user.send(f"Thank you for voting for <@1200756820403306586> on Top.GG.\nYou have received **10 extra lives** as a reward.\nYou now have **{extra_lives}** extra lives to use.\nDon't forget to vote again in 12 hours for more rewards!\n{user.mention}")
             return
