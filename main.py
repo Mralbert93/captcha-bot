5a16dac7378bfac52542b54fd231b689334fe442
Matthew Albert <61246987+Mralbert93@users.noreply.github.com>
2024-01-29 07:58:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ async def get_skips(player_id):
 async def check_roles(player_id):
     guild = bot.get_guild(1201163257461866596)
     player = guild.get_member(player_id)
+    print(player)
 
     global novice, apprentice, explorer, enthusiast, master, grandmaster, overlord, role_thresholds
     
