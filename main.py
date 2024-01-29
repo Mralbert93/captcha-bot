eb247582beab4ef1b60e469e5e9e33a3a019bbac
Matthew Albert <61246987+Mralbert93@users.noreply.github.com>
2024-01-29 09:36:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ async def check_roles(player_id, score, channel):
             await player.add_roles(role)
             embed = discord.Embed(
                 title="New Title Achieved",
-                description=f"<@{player_id}>\n\nCongratulations! You have received the **{role.name}** role by scoring more than {threshold}!",
+                description=f"<@{player_id}>\n\nCongratulations!\nYou earned the **{role.name}** role by scoring more than {threshold}!",
                 color=discord.Color.purple()
             )
             embed.set_thumbnail(url=bot.user.avatar.url)
